02e2c384bd24235e38cd7907d4e6aadc33a9704e
Germain <germain.gg90@gmail.com>
2024-07-21 23:29:22 -0400

    config morgan and create folders and files

--- server/app.ts
+++ server/app.ts
@@ -1,8 +1,10 @@
 import express, { json } from "express";
+import morgan from "morgan";
 
 export const app = express();
 app.use(json());
 app.disable("x-powered-by");
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello World" });
